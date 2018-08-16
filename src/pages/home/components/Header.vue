34ead9acb9cbba910c942555266c160e9b2f77f9
mfxy <mfxyzm@outlook.com>
2018-08-16 10:13:30 +0800

    Add Header in Home Page.

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <div class="header-left">back</div>
+    <div class="header-input">input</div>
+    <div class="header-right">city</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus"  scoped>
+  @import '~@/assets/styles/varibles.styl'
+  .header
+    background-color: $bgColor
+    color: #fff
+    display: flex
+    line-height: .86rem
+    .header-left
+      width: .64rem
+      float: left
+    .header-input
+      background-color: #fff
+      border-radius: .1rem
+      color: #ccc
+      flex: 1
+      line-height: .64rem
+      margin: .12rem 0
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+</style>

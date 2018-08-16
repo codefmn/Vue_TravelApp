034f26d90444711b821ffd08856bb131e5e285e9
mfxy <mfxyzm@outlook.com>
2018-08-17 06:15:42 +0800

    Add Swiper in Home.

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgSrc"/>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+import ImgSwiper1 from '@/assets/images/swiper1.png'
+import ImgSwiper2 from '@/assets/images/swiper2.png'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  name: 'HomeSwiper',
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgSrc: ImgSwiper1
+      }, {
+        id: '002',
+        imgSrc: ImgSwiper2
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff
+.wrapper
+  background: #eee
+  height: 0
+  overflow hidden
+  padding-bottom: 24.53%
+  width: 100%
+  .swiper-img
+    width: 100%
+</style>

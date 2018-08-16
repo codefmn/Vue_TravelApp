419a7d156dea7870bf439bc6482ca6074e764714
mfxy <mfxyzm@outlook.com>
2018-08-16 10:55:37 +0800

    Add icomoon.

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <div class="header-left">back</div>
-    <div class="header-input">input</div>
-    <div class="header-right">city</div>
+    <div class="header-left"><div class="icon-arrow-left2"></div></div>
+    <div class="header-input"><i class="icon-search"></i> Input to search</div>
+    <div class="header-right">City <i class="icon-circle-down"></i></div>
   </div>
 </template>
 
@@ -22,6 +22,9 @@ export default {
     .header-left
       width: .64rem
       float: left
+      .icon-arrow-left2
+        font-size: .4rem
+        margin: .2rem .1rem
     .header-input
       background-color: #fff
       border-radius: .1rem
@@ -29,6 +32,7 @@ export default {
       flex: 1
       line-height: .64rem
       margin: .12rem 0
+      padding-left: .2rem
     .header-right
       width: 1.24rem
       float: right

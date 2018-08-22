66024308b8b765cb9bc6497a0796933cb99171c1
mfxy <mfxyzm@outlook.com>
2018-08-23 01:20:57 +0800

    Add Header and Search componets in City page.

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <div class="header-left"><div class="icon-arrow-left2"></div></div>
+    </router-link>
+    城市选择
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+.header
+  background $bgColor
+  color #fff
+  font-size .4rem
+  height .9rem
+  line-height .9rem
+  text-align center
+  .icon-arrow-left2
+    color #fff
+    font-size .4rem
+    float left
+    margin .2rem .1rem
+    width .64rem
+</style>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="请输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+.search
+  background $bgColor
+  height .7rem
+  padding .2rem
+  input
+    border-radius .1rem
+    box-sizing border-box
+    line-height .65rem
+    text-align center
+    padding 0 .2rem
+    width 100%
+</style>

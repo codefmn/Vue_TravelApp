ab58704075f78b7539a86e3cfb7070b66f87e5fc
mfxy <mfxyzm@outlook.com>
2018-08-23 01:20:16 +0800

    Update router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -2,7 +2,9 @@
   <div class="header">
     <div class="header-left"><div class="icon-arrow-left2"></div></div>
     <div class="header-input"><i class="icon-search"></i> Input to search</div>
-    <div class="header-right">{{this.city}} <i class="icon-circle-down"></i></div>
+    <router-link to="/city">
+      <div class="header-right">{{this.city}} <i class="icon-circle-down"></i></div>
+    </router-link>
   </div>
 </template>
 
@@ -18,26 +20,27 @@ export default {
 <style lang="stylus"  scoped>
   @import '~@/assets/styles/varibles.styl'
   .header
-    background-color: $bgColor
-    color: #fff
-    display: flex
-    line-height: .86rem
+    background-color $bgColor
+    color #fff
+    display flex
+    line-height .9rem
     .header-left
-      width: .64rem
-      float: left
+      float left
+      width .64rem
       .icon-arrow-left2
-        font-size: .4rem
-        margin: .2rem .1rem
+        font-size .4rem
+        margin .2rem .1rem
     .header-input
-      background-color: #fff
-      border-radius: .1rem
-      color: #ccc
-      flex: 1
-      line-height: .64rem
-      margin: .12rem 0
-      padding-left: .2rem
+      background-color #fff
+      border-radius .1rem
+      color #ccc
+      flex 1
+      line-height .64rem
+      margin .12rem 0
+      padding-left .2rem
     .header-right
-      width: 1.24rem
-      float: right
-      text-align: center
+      color #fff
+      float right
+      text-align center
+      width 1.24rem
 </style>

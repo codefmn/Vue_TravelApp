e54ffccd7b4e74aef0185a7503085c6c23303182
mfxy <mfxyzm@outlook.com>
2018-09-01 23:38:07 +0800

    Add Detail page.

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <detail-banner
+      :bannerImg="bannerImg"
+      :sightName="sightName"
+    ></detail-banner>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import DetailBanner from './components/Banner'
+export default {
+  name: 'Detail',
+  components: {
+    DetailBanner
+  },
+  data () {
+    return {
+      bannerImg: '',
+      sightName: ''
+    }
+  },
+  mounted () {
+    axios.get('api/detail.json')
+      .then(response => response.data)
+      .then(response => {
+        if (response.ret && response.data) {
+          this.bannerImg = response.data.bannerImg
+          this.sightName = response.data.sightName
+        }
+      })
+  }
+}
+</script>
